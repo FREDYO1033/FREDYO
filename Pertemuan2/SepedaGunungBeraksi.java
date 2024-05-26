8320a2d63bb53458c7daed0c963e1ad8318a0b49
FREDYO <fredyolumbanraja1033@gmail.com>
2024-05-26 16:12:36 +0700

    praktikum 2

---
+++ Pertemuan2/SepedaGunungBeraksi.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sepedagunungberaksi;
+
+/**
+ *
+ * @author fredy
+ */
+public class SepedaGunungBeraksi {
+
+     public static void main(String[] args) {
+        // Membuat object
+        SepedaGunung sepedaku = new SepedaGunung();
+        // Memanggil method di object
+        sepedaku.tambahKecepatan(10);
+        sepedaku.ubahGir(2);
+        sepedaku.setSadel(20);
+    }
+    
+}
