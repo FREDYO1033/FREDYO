d3f8c543598e6374512ad14bf53ba63a2188cbfc
FREDYO <fredyolumbanraja1033@gmail.com>
2024-05-26 15:58:45 +0700

    praktikum 1

---
+++ LatihanVariabel.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package KonstantaDanKomentar;
+
+/**
+ *
+ * @author fredy
+ */
+public class LatihanVariabel {
+
+     int jarijari;
+        double keliling;
+        final double PI=3.14;
+        
+    {
+        jarijari=30;
+        
+        keliling=2*PI*jarijari;
+        System.out.println("Keliling Jari-jari = "+keliling);
+       System.out.println("======================");
+    }
+    
+}
